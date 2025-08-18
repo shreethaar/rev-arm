90faef7309c594595b302c7cfd2af136d82640cf
shreethaar <shreethaara380@gmail.com>
2025-08-19 07:16:41 +0800

    asm for arm not knee, arm wokay

---
+++ week3_arm_asm/arm1.s
@@ -0,0 +1,27 @@
+	.file	"arm1.c"
+	.text
+	.globl	main                            // -- Begin function main
+	.p2align	2
+	.type	main,@function
+main:                                   // @main
+	.cfi_startproc
+// %bb.0:
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	str	wzr, [sp, #12]
+	mov	w8, #2                          // =0x2
+	str	w8, [sp, #8]
+	ldr	w8, [sp, #8]
+	ldr	w9, [sp, #8]
+	add	w8, w8, w9
+	str	w8, [sp, #4]
+	ldr	w0, [sp, #4]
+	add	sp, sp, #16
+	.cfi_def_cfa_offset 0
+	ret
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        // -- End function
+	.ident	"clang version 20.1.8"
+	.section	".note.GNU-stack","",@progbits
